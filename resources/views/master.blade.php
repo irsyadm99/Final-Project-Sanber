040fe3614641bd0bc431fd96a93cffbdea3adc4d
Anastasia Victoria <victoriatasia@gmail.com>
2020-07-09 22:37:38 +0700

    master template

---
+++ resources/views/master.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <style>
+        footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #404040;
+            color: white;
+            text-align: center;
+        }
+    </style>
+
+    <title>template</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+        <nav class="navbar fixed-top navbar-light bg-light px-4 py-2">
+            <a class="navbar-brand">LOGO</a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <form class="form-inline">
+                <button class="btn btn-outline-success mx-2" type="button">Log In</button>
+                <button class="btn btn-outline-success mx-2" type="button">Sign Up</button>
+              </form>
+        </nav>
+    </div>
+    <div class="container-fluid mt-5 pt-2">
+        <div class="row">
+            <div class="col-2 border-right text-right px-4 py-3">
+               <a href="/">Home</a><br>
+               <a href="/">Pertanyaan</a>
+            </div>
+            <div class="col px-4 py-3">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+    <footer class="p-3">© 2020</footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
