bce3b6f4bc32bcdfa9b6e251388f9f955f819743
Anastasia Victoria <victoriatasia@gmail.com>
2020-07-10 10:19:06 +0700

    pertanyaan

---
+++ resources/views/pertanyaan.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+
+@section('content')
+    <h2>
+        Semua Pertanyaan
+        <a href="/"><button class="btn btn-success float-right" type="button">Pertanyaan Baru</button></a>
+    </h2>
+    @foreach($pertanyaan as $key=> $pertanyaan)
+    <hr>
+    <div class="row">
+        <div class="col-1 text-center text-muted">
+            <h4>0</h4>
+            <p>votes</p>
+            <h4>0</h4>
+            <p>jawaban</p>
+        </div>
+        <div class="col">
+            <b>{{$pertanyaan-> judul}}</b>
+            <p>Jawaban</p>
+        </div>
+    </div>
+    @endforeach
+@endsection
